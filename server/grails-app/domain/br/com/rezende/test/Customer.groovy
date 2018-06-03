78595b9b1757e0ecc6c03025aa2223671a1b0351
Andre Rezende <homerezende@gmail.com>
2018-06-03 11:17:29 -0300

    Projeto de exemplo de Grails com Angular 5, aumentando produtividade e qualidade de código.

---
+++ server/grails-app/domain/br/com/rezende/test/Customer.groovy
@@ -0,0 +1,26 @@
+package br.com.rezende.test
+
+import grails.rest.*
+
+@Resource(uri='/customer')
+class Customer {
+
+  String name
+  String address
+  String city
+  String postalCode
+  String phone
+
+  static constraints = {
+      name blank:false
+      address blank:false
+      city blank:false
+      postalCode blank:false
+      phone blank:false
+  }
+
+  String toString() {
+    name
+  }
+ 
+}
